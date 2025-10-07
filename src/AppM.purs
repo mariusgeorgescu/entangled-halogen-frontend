75675bb629fba24787c8d3f916c365f77137b4cb
Marius Georgescu <georgescumarius@live.com>
2025-10-07 22:04:06 +0300

    init

---
+++ src/AppM.purs
@@ -0,0 +1,89 @@
+module AppM
+  ( AppM
+  , getDecodedJson
+  , runAppM
+  ) where
+
+import Prelude
+import AppEnv (Env)
+import Capabilities.MonadCIP30 (class MonadCIP30)
+import Cardano.Wallet.Cip30 as Cip30
+import Control.Monad.Error.Class (class MonadThrow)
+import Control.Monad.Reader (class MonadReader, ReaderT, runReaderT)
+import Control.Monad.Reader.Class (class MonadAsk)
+import Data.Argonaut.Decode (JsonDecodeError, printJsonDecodeError)
+import Data.Either (Either, either)
+import Effect (Effect)
+import Effect.Aff (Aff, Error)
+import Effect.Aff.Class (class MonadAff)
+import Effect.Class (class MonadEffect)
+import Effect.Exception (throw)
+import Halogen as H
+import Halogen.Store.Monad (class MonadStore, StoreT, runStoreT)
+import Safe.Coerce (coerce)
+import Store as Store
+
+------
+newtype AppM a
+  = AppM (ReaderT Env (StoreT Store.Action Store.Store Aff) a)
+
+runAppM ::
+  forall q i o.
+  Env ->
+  Store.Store ->
+  H.Component q i o AppM ->
+  Aff (H.Component q i o Aff)
+runAppM env initialStore rootComponent =
+  runStoreT initialStore Store.reduce
+    $ (H.hoist hoistToAff (coerce rootComponent))
+  where
+  hoistToAff :: forall a. ReaderT Env (StoreT Store.Action Store.Store Aff) a -> (StoreT Store.Action Store.Store Aff a)
+  hoistToAff m = runReaderT m env
+
+derive newtype instance functorAppM :: Functor AppM
+
+derive newtype instance applyAppM :: Apply AppM
+
+derive newtype instance applicativeAppM :: Applicative AppM
+
+derive newtype instance bindAppM :: Bind AppM
+
+derive newtype instance monadAppM :: Monad AppM
+
+derive newtype instance monadEffectAppM :: MonadEffect AppM
+
+derive newtype instance monadAffAppM :: MonadAff AppM
+
+derive newtype instance monadStoreAppM :: MonadStore Store.Action Store.Store AppM
+
+derive newtype instance monadErrorAppM :: MonadThrow Error AppM
+
+derive newtype instance monadReaderAppM :: MonadReader Env AppM
+
+derive newtype instance monadAskAppM :: MonadAsk Env AppM
+
+getDecodedJson ∷ ∀ a. Either JsonDecodeError a → Effect a
+getDecodedJson = either (throw <<< printJsonDecodeError) pure
+
+-- MonadCIP30 instance required by external WalletConnect component
+instance monadCip30AppM :: MonadCIP30 AppM where
+  enable w exts = H.liftAff $ Cip30.enable w exts
+  getExtensions = H.liftAff <<< Cip30.getExtensions
+  getNetworkId = H.liftAff <<< Cip30.getNetworkId
+  getBalance = H.liftAff <<< Cip30.getBalance
+  getUsedAddresses api mp = H.liftAff $ Cip30.getUsedAddresses api mp
+  getUtxos api ma mp = H.liftAff $ Cip30.getUtxos api ma mp
+  getCollateral api amt = H.liftAff $ Cip30.getCollateral api amt
+  getUnusedAddresses = H.liftAff <<< Cip30.getUnusedAddresses
+  getChangeAddress = H.liftAff <<< Cip30.getChangeAddress
+  getRewardAddresses = H.liftAff <<< Cip30.getRewardAddresses
+  signTx api tx isPartial = H.liftAff $ Cip30.signTx api tx isPartial
+  signData api addr payload = H.liftAff $ Cip30.signData api addr payload
+  submitTx api tx = H.liftAff $ Cip30.submitTx api tx
+  getName = H.liftEffect <<< Cip30.getName
+  getIcon = H.liftEffect <<< Cip30.getIcon
+  isWalletAvailable = H.liftEffect <<< Cip30.isWalletAvailable
+  getAvailableWallets = H.liftEffect Cip30.getAvailableWallets
+  isEnabled = H.liftAff <<< Cip30.isEnabled
+  getApiVersion = H.liftEffect <<< Cip30.getApiVersion
+  getSupportedExtensions = H.liftEffect <<< Cip30.getSupportedExtensions
---
+++ src/Components/NavBar.purs
@@ -0,0 +1,144 @@
+module Components.NavBar where
+
+import Prelude
+import AppEnv (Env)
+import Capabilities.MonadCIP30 (class MonadCIP30)
+import Cardano.Wallet.Cip30 (Api)
+import WalletConnect.Component as WC
+import Control.Monad.Reader.Class (class MonadAsk)
+import Data.Maybe (Maybe(..))
+import Effect.Aff.Class (class MonadAff)
+import Halogen as H
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
+-- import Halogen.HTML.Properties.ARIA as HPA -- no longer used here
+import Halogen.Store.Connect (Connected, connect)
+import Halogen.Store.Monad (class MonadStore, updateStore)
+import Halogen.Store.Select (selectAll)
+import Store as Store
+import Type.Proxy (Proxy(..))
+
+--------------------------------------------------------------------------------
+-- * Component Interface
+--------------------------------------------------------------------------------
+type Slot
+  = forall query. H.Slot query Output Unit
+
+navbarProxy = Proxy :: Proxy "navbarWidget"
+
+type Input
+  = Unit
+
+type StoreContext
+  = Store.Store
+
+data Output
+  = WalletConnectEvent
+  | InvalidNetworkEvent
+  | HomeEvent
+
+--------------------------------------------------------------------------------
+-- * Child Slots
+--------------------------------------------------------------------------------
+type Slots
+  = ( walletConnectComponent :: H.Slot WC.Query WC.Output Unit
+    )
+
+--------------------------------------------------------------------------------
+-- * Component Definition
+--------------------------------------------------------------------------------
+type State
+  = { walletApi :: Maybe Api
+    }
+
+data Action
+  = Initialize
+  | Receive (Connected StoreContext Input)
+  | HandleWalletConnectOutput WC.Output
+  | HomeButton
+
+component ::
+  forall query m.
+  MonadAff m =>
+  MonadCIP30 m =>
+  MonadAsk Env m =>
+  MonadStore Store.Action Store.Store m =>
+  H.Component query Input Output m
+component =
+  connect (selectAll)
+    $ H.mkComponent
+        { initialState
+        , render
+        , eval:
+            H.mkEval
+              H.defaultEval
+                { handleAction = handleAction
+                , initialize = Just Initialize
+                , receive = Just <<< Receive
+                }
+        }
+
+--------------------------------------------------------------------------------
+-- * Component Evalution Logic
+--------------------------------------------------------------------------------
+initialState :: Connected StoreContext Input -> State
+initialState x =
+  { walletApi: x.context.walletApi
+  }
+
+handleAction ::
+  forall m.
+  MonadAff m =>
+  MonadCIP30 m =>
+  MonadAsk Env m =>
+  MonadStore Store.Action Store.Store m =>
+  Action → H.HalogenM State Action Slots Output m Unit
+handleAction = case _ of
+  Initialize -> do
+    walletApi <- H.gets _.walletApi
+    void $ H.query WC.walletConnectProxy unit (WC.SetWalletApi walletApi unit)
+  Receive x -> do
+    H.modify_ _ { walletApi = x.context.walletApi }
+    handleAction Initialize
+  HandleWalletConnectOutput out -> case out of
+    WC.WalletConnectedEvent -> H.raise WalletConnectEvent
+    WC.WalletDisconnectedEvent -> do
+      updateStore Store.Disconnect
+      H.raise WalletConnectEvent
+    WC.CustomButtonEvent bid -> case bid of
+      "home" -> H.raise HomeEvent
+      "delegate" -> pure unit
+      _ -> pure unit
+  HomeButton -> H.raise HomeEvent
+
+--------------------------------------------------------------------------------
+-- * Component Rendering
+--------------------------------------------------------------------------------
+render :: forall m. MonadAff m => MonadCIP30 m => State -> H.ComponentHTML Action Slots m
+render state =
+  HH.div
+    [ HP.classes [ HH.ClassName "bg-base-100 text-base-content sticky top-0 z-30 flex h-16 w-full justify-center bg-opacity-90 backdrop-blur transition-shadow duration-100 [transform:translate3d(0,0,0)] shadow-sm" ]
+    ]
+    [ HH.div
+        [ HP.classes [ HH.ClassName "navbar bg-neutral text-neutral-content gap-4" ] ]
+        [ HH.div [ HP.classes [ HH.ClassName "flex-1" ] ]
+            [ HH.button
+                ([ HP.classes [ HH.ClassName ("btn btn-ghost") ], HE.onClick (\_ -> HomeButton) ])
+                [ HH.div [ HP.classes [ HH.ClassName "text-lg" ] ]
+                    [ HH.text "E7D" ]
+                , HH.div [ HP.classes [ HH.ClassName "text-xs" ] ]
+                    [ HH.text " </alpha>" ]
+                ]
+            ]
+        , HH.div [ HP.classes [ HH.ClassName "flex-2 flex justify-end" ] ]
+            [ HH.slot WC.walletConnectProxy unit WC.component { buttons: customButtons, assets: { connectIcon: "./images/walletsymbol.svg", disconnectIcon: "./images/disconnectsymbol.svg" } } HandleWalletConnectOutput
+            ]
+        , HH.div [ HP.classes [ HH.ClassName "flex-none" ] ] []
+        ]
+    ]
+  where
+  customButtons =
+    [ { id: "home", label: "Home", iconSrc: "./images/home-symbol.svg", classes: [ "btn-secondary" ] }
+    , { id: "delegate", label: "Delegate", iconSrc: "./images/createsymbol.svg", classes: [ "btn-primary" ] }
+    ]
